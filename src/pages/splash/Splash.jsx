55d0526b48e9b0b5ef49ef146869c37babafb394
DILIP <ydk088@gmail.com>
2023-06-29 18:20:46 +0530

    Splash Screen

--- src/pages/splash/Splash.jsx
+++ src/pages/splash/Splash.jsx
@@ -1,10 +1,23 @@
 import './Splash.css'
 import '../../index.css'
+import Logo from '../../image/logo.png'
+import { useEffect } from 'react'
+import { Link } from "react-router-dom";
 
-function Splash(){
-    return(
-        <div className="splash">
+function Splash() {
 
+    useEffect(() => {
+        setTimeout(() => {
+            <Link to={"/dictionary"}>
+                <button className='back'>Back</button>
+            </Link>
+        }, 1000);
+    }, [])
+
+
+    return (
+        <div className="splash | flex fx-ai-c fx-jc-c">
+            <img src={Logo} alt="" />
         </div>
     )
 }
